72a38d1b5f3b5ac4fdca9740614ca12fec9609c1
verlinof <ajikoko.zx@gmail.com>
2025-01-20 10:41:11 +0700

    Feat: Goose Config, Error Standard, Success Standard

--- pkg/error/main.go
+++ pkg/error/main.go
@@ -1,1 +1,45 @@
-package error
+package pkg_error
+
+import (
+	"net/http"
+)
+
+type ClientError struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func NewBadRequest(err error) *ClientError {
+	return &ClientError{
+		Status:  http.StatusBadRequest,
+		Message: err.Error(),
+	}
+}
+
+func NewInternalServerError(err error) *ClientError {
+	return &ClientError{
+		Status:  http.StatusInternalServerError,
+		Message: err.Error(),
+	}
+}
+
+func NewNotFound(err error) *ClientError {
+	return &ClientError{
+		Status:  http.StatusNotFound,
+		Message: err.Error(),
+	}
+}
+
+func NewForbidden(err error) *ClientError {
+	return &ClientError{
+		Status:  http.StatusForbidden,
+		Message: err.Error(),
+	}
+}
+
+func NewUnauthorized(err error) *ClientError {
+	return &ClientError{
+		Status:  http.StatusUnauthorized,
+		Message: err.Error(),
+	}
+}
--- pkg/success/main.go
+++ pkg/success/main.go
@@ -1,1 +1,29 @@
-package success
+package pkg_success
+
+import "fmt"
+
+type ClientSuccess struct {
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+func SuccessGetData(data any) *ClientSuccess {
+	return &ClientSuccess{
+		Message: "Success",
+		Data:    data,
+	}
+}
+
+func SuccessDeleteData(id any) *ClientSuccess {
+	return &ClientSuccess{
+		Message: fmt.Sprintf("Success delete data with ID %v", id),
+		Data:    id,
+	}
+}
+
+func SuccessCreateData(data interface{}) *ClientSuccess {
+	return &ClientSuccess{
+		Message: "Success",
+		Data:    data,
+	}
+}
